1a089877314216b7f566b41d432ebbe1f0a3e789
Ronit Virwani <ronitvirwani@192.168.1.4>
2025-12-12 04:42:31 +0530

    Remove utils/ from tracking

--- utils/cv_check.py
+++
@@ -1,9 +0,0 @@
-
-# save as check_cv.py and run: python3 check_cv.py
-import cv2, re
-info = cv2.getBuildInformation()
-print("AVFoundation present? ", bool(re.search(r'AVFoundation:\s+YES', info)))
-print("\n=== Video I/O block (first 1200 chars) ===\n")
-start = info.find("Video I/O:")
-print(info[start:start+1200])
-
